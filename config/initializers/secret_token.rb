11fac885e7c606fdbea935ec0e9925f22c8395c6
Steven Kyle Cook <skc2134@columbia.edu>
2012-12-29 11:19:23 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PigeonApp::Application.config.secret_token = '1715031c92ea4225ca203d166f34c1c49f011779a575f0c2dbf36648eb50d7f9df50d7567cea4e967a15775471d5c0414465788d294f230753e89ddbdcd7f600'
